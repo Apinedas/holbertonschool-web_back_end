895e80984024f13479639734a5bca67390336eaa
Alejandro Pineda Sánchez <apsanchez2549@gmail.com>
2022-10-09 17:42:09 -0500

    fix: Second try on 4

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,16 +1,12 @@
 #!/usr/bin/env python3
-"""First Async and Await corroutine"""
-
-from typing import List
+"""Async Tasks"""
 import asyncio
-wait_task = __import__('3-tasks').task_wait_random
+from typing import List
+
+task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """A cycle with an async function"""
-    delay_list = [wait_task(max_delay) for i in range(0, n)]
-    ret_list = []
-    for w_random in asyncio.as_completed(delay_list):
-        delay = await w_random
-        ret_list.append(delay)
-    return ret_list
+    """Returns a list of delayed floats by using tasks"""
+    tasks = [task_wait_random(max_delay) for i in range(n)]
+    return [await t for t in asyncio.as_completed(tasks)]
