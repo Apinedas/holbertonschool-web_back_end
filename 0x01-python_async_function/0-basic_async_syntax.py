472750b6ea98536a1becf27a57b06286b3178f5a
Alejandro Pineda Sánchez <apsanchez2549@gmail.com>
2022-10-09 05:17:46 -0500

    feat: Added Python Async

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""First Async and Await corroutine"""
+
+
+import asyncio
+import random
+
+async def wait_random(max_delay: int = 10) -> float:
+    """First concurrent module"""
+    i = random.uniform(0, max_delay)
+    await asyncio.sleep(i)
+    return i
