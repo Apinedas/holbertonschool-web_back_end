b1c9bd93c24ce62a310820e59389a44ef185cca4
Alejandro Pineda Sánchez <apsanchez2549@gmail.com>
2022-10-09 05:39:12 -0500

    fix: Anotation

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 """First Async and Await corroutine"""
 
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list:
+async def wait_n(n: int, max_delay: int) -> List:
     """A cycle with an async function"""
     ret_list = []
     for i in range(0, n):
