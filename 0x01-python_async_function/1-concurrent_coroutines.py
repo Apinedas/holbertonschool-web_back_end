ad1c4f7bb6a4ff306d8e263d116e443da0dc27b2
Alejandro Pineda Sánchez <apsanchez2549@gmail.com>
2022-10-09 06:07:21 -0500

    fix: removed sort

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -2,14 +2,15 @@
 """First Async and Await corroutine"""
 
 from typing import List
+import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """A cycle with an async function"""
+    delay_list = [wait_random(max_delay) for i in range(0, n)]
     ret_list = []
-    for i in range(0, n):
-        delay = await wait_random(max_delay)
+    for w_random in asyncio.as_completed(delay_list):
+        delay = await w_random
         ret_list.append(delay)
-    ret_list.sort()
     return ret_list
