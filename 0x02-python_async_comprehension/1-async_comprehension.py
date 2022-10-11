f8dfbd324e794e39efa3f452d87c1b7ced0c67d3
Alejandro Pineda Sánchez <apsanchez2549@gmail.com>
2022-10-11 14:44:32 -0500

    fix: Doc

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -6,4 +6,5 @@ async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
+    '''Generates a List from a comprehended async function'''
     return [i async for i in async_generator()]
