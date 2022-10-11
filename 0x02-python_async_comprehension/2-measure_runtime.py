71593d188b08acb232cae73727dddb7e64b7ced8
Alejandro Pineda Sánchez <apsanchez2549@gmail.com>
2022-10-11 14:57:21 -0500

    fix: Second try on last

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -8,11 +8,7 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 async def measure_runtime() -> float:
     '''Measures runtime of four runs of async comprehension'''
     start = time.perf_counter()
-    await asyncio.gather(
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension()
-    )
+    await asyncio.gather(async_comprehension(), async_comprehension(),
+        async_comprehension(), async_comprehension())
     total_time = time.perf_counter() - start
     return total_time
