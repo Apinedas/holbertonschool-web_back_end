c664d67d4bf9d9380b9e82aac4d14c92da9fac53
Alejandro Pineda Sánchez <apsanchez2549@gmail.com>
2022-10-11 14:28:51 -0500

    fix: Doc and style

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -5,7 +5,9 @@ import asyncio
 import random
 from typing import Generator
 
-async def async_generator () -> Generator[int, None, None]:
+
+async def async_generator () -> Generator[float, None, None]:
+    '''Generator for a 10 iterations random number'''
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
