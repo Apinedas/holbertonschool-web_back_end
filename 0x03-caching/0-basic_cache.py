746501349ec0ab236bed5435fb54e431b1ab454e
Alejandro Pineda Sánchez <apsanchez2549@gmail.com>
2022-10-13 12:41:12 -0500

    fix: Doc

--- 0x03-caching/0-basic_cache.py
+++ 0x03-caching/0-basic_cache.py
@@ -7,12 +7,15 @@ from base_caching import BaseCaching
 class BasicCache(BaseCaching):
     '''Basic cache class'''
     def __init__(self) -> None:
+        '''Class init method'''
         super().__init__()
 
     def put(self, key, item):
+        '''Class put method: Puts an item on the dict'''
         if key and item:
             self.cache_data[key] = item
 
     def get(self, key):
+        '''Class get method: Gets an item from the dict'''
         if key and key in self.cache_data.keys():
             return (self.cache_data[key])
