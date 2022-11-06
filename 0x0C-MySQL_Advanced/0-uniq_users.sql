e55feae442e1fdef2bd2ac8ed794b8356213e1ed
Alejandro Pineda Sanchez <apsanchez2549@gmail.com>
2022-11-06 18:58:51 -0500

    fix: Changed 0

--- 0x0C-MySQL_Advanced/0-uniq_users.sql
+++ 0x0C-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,6 @@
 -- Creating a specific table
 CREATE TABLE IF NOT EXISTS users(
     id INT NOT NULL AUTO_INCREMENT PRIMARY_KEY,
-    email VARCHAR(255) NOT NULL UNIQUE,
+    email VARCHAR(255) UNIQUE NOT NULL,
     name VARCHAR(255)
 )
