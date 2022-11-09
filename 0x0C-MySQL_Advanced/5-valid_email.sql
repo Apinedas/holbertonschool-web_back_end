c93c8fb31b66281a3d6e6bcecb63367558059e2b
Alejandro Pineda Sanchez <apsanchez2549@gmail.com>
2022-11-09 00:10:45 -0500

    feat: Added 5

---
+++ 0x0C-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,4 @@
+-- Valid email
+DELIMITER $$
+CREATE TRIGGER check_email BEFORE UPDATE ON users FOR EACH ROW BEGIN IF OLD.email <> NEW.email THEN SET NEW.valid_email = 0; END IF; END;$$
+DELIMITER ;
