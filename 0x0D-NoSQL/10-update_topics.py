fcea8ee75a945af032600c99deda1b2afcde89e3
Alejandro Pineda Sanchez <apsanchez2549@gmail.com>
2022-11-14 18:11:51 -0500

    fix: Changed 10

--- 0x0D-NoSQL/10-update_topics.py
+++ 0x0D-NoSQL/10-update_topics.py
@@ -5,5 +5,5 @@
 def update_topics(mongo_collection, name, topics):
     """Updates document"""
     to_update = {'name': name}
-    to_add = {'topics': topics}
+    to_add = { "$set": {'topics': topics} }
     mongo_collection.update_many(to_update, to_add)
