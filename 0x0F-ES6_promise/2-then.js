2a4a7bbbfbe297471f1d12a4ff70211a299f560e
Alejandro Pineda Sanchez <apsanchez2549@gmail.com>
2022-11-16 20:50:25 -0500

    fix: Another try

--- 0x0F-ES6_promise/2-then.js
+++ 0x0F-ES6_promise/2-then.js
@@ -1,11 +1,10 @@
 export default function handleResponseFromAPI(promise) {
-  return promise.then(() => {
-    const retObject = {
+  return promise.then(() => (
+    {
       status: 200,
       body: 'success',
-    };
-    return retObject;
-  })
+    }
+  ))
     .catch(() => Error())
     .then(() => {
       console.log('Got a response from the API');
