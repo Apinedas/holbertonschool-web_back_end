02615df4ee4d8edce88156afbb9a7032c5359ec6
Alejandro Pineda Sanchez <apsanchez2549@gmail.com>
2022-11-16 19:48:43 -0500

    fix: Updated 0

--- 0x0F-ES6_promise/0-promise.js
+++ 0x0F-ES6_promise/0-promise.js
@@ -1,3 +1,4 @@
-export function getResponseFromAPI() {
-    return new Promise ((resolve, reject) => {})
+export default function getResponseFromAPI() {
+  /* eslint-disable */
+  return new Promise((resolve, reject) => {});
 }
