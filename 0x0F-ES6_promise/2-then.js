ede1dce8c8f53e043e3d5d43f00c51d2a8723acd
Alejandro Pineda Sanchez <apsanchez2549@gmail.com>
2022-11-16 20:42:40 -0500

    fix: Third try

--- 0x0F-ES6_promise/2-then.js
+++ 0x0F-ES6_promise/2-then.js
@@ -5,9 +5,10 @@ export default function handleResponseFromAPI(promise) {
       body: 'success',
     };
     return retObject;
-  })
-  promise.catch(() => Error())
+  });
+  promise.catch(() => Error());
   promise.finally(() => {
-      console.log('Got a response from the API');
-    });
+    console.log('Got a response from the API');
+  });
+  return promise;
 }
