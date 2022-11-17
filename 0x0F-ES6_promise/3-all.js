bf949c1f292c39ee7ad1050c8d30ec51ef34705b
Alejandro Pineda Sanchez <apsanchez2549@gmail.com>
2022-11-16 21:23:50 -0500

    second try

--- 0x0F-ES6_promise/3-all.js
+++ 0x0F-ES6_promise/3-all.js
@@ -3,7 +3,7 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   const photoResult = uploadPhoto();
   const userResult = createUser();
-  Promise.all([photoResult, userResult]).then((resolveArr) => {
+  return Promise.all([photoResult, userResult]).then((resolveArr) => {
     console.log(`${resolveArr[0].body} ${resolveArr[1].firstName} ${resolveArr[1].lastName}`);
   })
     .catch(() => {
