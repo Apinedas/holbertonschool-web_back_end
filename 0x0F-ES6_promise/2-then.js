7ae96b52afbc601bc30d9a3037d9e80c950f5fd6
Alejandro Pineda Sanchez <apsanchez2549@gmail.com>
2022-11-16 20:29:59 -0500

    feat: Added 2

---
+++ 0x0F-ES6_promise/2-then.js
@@ -0,0 +1,13 @@
+export default function handleResponseFromAPI(promise) {
+  promise.then(() => {
+    const retObject = {
+      status: 200,
+      body: 'success',
+    };
+    return retObject;
+  })
+    .catch(() => Error())
+    .then(() => {
+      console.log('Got a response from the API');
+    });
+}
