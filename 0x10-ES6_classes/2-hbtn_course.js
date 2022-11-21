abff50e5005c8d6b8586342d0ee8c6a6c95c7964
Alejandro Pineda Sanchez <apsanchez2549@gmail.com>
2022-11-20 20:37:09 -0500

    fix: Added list check

--- 0x10-ES6_classes/2-hbtn_course.js
+++ 0x10-ES6_classes/2-hbtn_course.js
@@ -1,10 +1,13 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
     if (typeof (name) !== 'string') {
-      throw (new TypeError('Name must be a string'));
+      throw (new Error('Name must be a string'));
     }
     if (typeof (length) !== 'number') {
-      throw (new TypeError('Length must be a number'));
+      throw (new Error('Length must be a number'));
+    }
+    if (typeof (students) !== 'object') {
+      throw (new Error('Students must be an object'));
     }
     this._name = name;
     this._length = length;
@@ -38,6 +41,9 @@ export default class HolbertonCourse {
   }
 
   set students(students) {
+    if (typeof (students) !== 'object') {
+      throw (new Error('Students must be an object'));
+    }
     this._students = students;
   }
 }
