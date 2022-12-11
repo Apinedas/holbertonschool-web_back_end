a4afb19bcc1152fc450bbbfef2d4d6e6c744b7c4
Alejandro Pineda Sanchez <apsanchez2549@gmail.com>
2022-12-11 13:59:05 -0500

    feat: Added 1

---
+++ 0x12-Node_JS_basic/1-stdin.js
@@ -0,0 +1,14 @@
+process.stdin.setEncoding('utf8');
+console.log('Welcome to Holberton School, what is your name?')
+
+
+process.stdin.on('readable', function() {
+  const name = process.stdin.read();
+  if (name !== null) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
+});
+
+process.stdin.on('end', function() {
+  console.log('This important software is now closing');
+});
