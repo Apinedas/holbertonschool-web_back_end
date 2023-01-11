882e07a05ed5a4afc6d7a41c478257c6a8b7531e
Alejandro Pineda Sanchez <apsanchez2549@gmail.com>
2023-01-10 19:18:26 -0500

    feat: Typo correction

--- 0x13-unittests_in_js/1-calcul.test.js
+++ 0x13-unittests_in_js/1-calcul.test.js
@@ -18,19 +18,19 @@ describe('calculateNumber', () => {
         assert.equal(calculateNumber('SUM', 1.2, 3.4), 4);
     });
     it('should return 0 when both numbers are 0', () => {
-        assert.equal(calculateNumber('SUBSTRACT', 0, 0), 0);
+        assert.equal(calculateNumber('SUBTRACT', 0, 0), 0);
     });
     it('should return 0 when both numbers are opposite', () => {
-        assert.equal(calculateNumber('SUBSTRACT', 5, -5), 10);
+        assert.equal(calculateNumber('SUBTRACT', 5, -5), 10);
     });
     it('should round numbers', () => {
-        assert.equal(calculateNumber('SUBSTRACT', 1, 3.2), -2);
+        assert.equal(calculateNumber('SUBTRACT', 1, 3.2), -2);
     });
     it('should round numbers', () => {
-        assert.equal(calculateNumber('SUBSTRACT', 1.2, 3), -2);
+        assert.equal(calculateNumber('SUBTRACT', 1.2, 3), -2);
     });
     it('should round numbers', () => {
-        assert.equal(calculateNumber('SUBSTRACT', 1.2, 3.4), -2);
+        assert.equal(calculateNumber('SUBTRACT', 1.2, 3.4), -2);
     });
     it('should return 0 when both numbers are 0', () => {
         assert.equal(calculateNumber('DIVIDE', 0, 0), 'Error');
