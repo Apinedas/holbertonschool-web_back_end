7eb7f50526725f59e4c8e51b58ed3732c4429e46
Alejandro Pineda Sánchez <apsanchez2549@gmail.com>
2023-05-24 15:41:18 -0500

    fix: Style

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -2,12 +2,14 @@
 '''Module to hash a password and store it on the DB'''
 import bcrypt
 
+
 def hash_password(password: str) -> bytes:
     '''Function to hash a password'''
     password = password.encode('utf-8')
     hashed_password = bcrypt.hashpw(password, bcrypt.gensalt())
-    return(hashed_password)
+    return (hashed_password)
+
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
     '''Function to check a password'''
-    return(bcrypt.checkpw(password.encode('utf-8'), hashed_password))
+    return (bcrypt.checkpw(password.encode('utf-8'), hashed_password))
