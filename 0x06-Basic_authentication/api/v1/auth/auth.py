6d5ce5b6a1280b5b3226efb4a8a3d239bfc96c59
Alejandro Pineda Sanchez <apsanchez2549@gmail.com>
2023-05-30 12:55:38 -0500

    feat: Added Auth

---
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Auth class for basic authentication"""
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth():
+    """Auth class for basic authentication"""
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        return False
+    
+
+    def authorization_header(self, request=None) -> str:
+        return None
+    
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        return None
