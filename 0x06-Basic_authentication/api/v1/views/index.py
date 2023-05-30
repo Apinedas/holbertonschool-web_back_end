b57f0f4c23331401fd8cac1b98d04a413140135b
Alejandro Pineda Sanchez <apsanchez2549@gmail.com>
2023-05-30 12:30:42 -0500

    feat: Added task 2

--- 0x06-Basic_authentication/api/v1/views/index.py
+++ 0x06-Basic_authentication/api/v1/views/index.py
@@ -32,3 +32,11 @@ def unauthorized():
     Return:
       - Error 401"""
     abort(401)
+
+
+@app_views.route('/forbidden', strict_slashes=False)
+def forbidden():
+    """ GET /api/v1/forbidden
+    Return:
+      - Error 403"""
+    abort(403)
