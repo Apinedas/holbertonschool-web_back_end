3ff6f490bc2b0f8a538cd222516ce541c1fe8be7
Alejandro Pineda Sanchez <apsanchez2549@gmail.com>
2023-05-31 11:53:30 -0500

    feat: Added 4

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -9,6 +9,10 @@ class Auth():
     """Auth class for basic authentication"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Require auth"""
+        if path and path[-1] != "/":
+            path += "/"
+        if path is None or excluded_paths is None or len(excluded_paths) == 0 or path not in excluded_paths:
+            return True
         return False
 
     def authorization_header(self, request=None) -> str:
