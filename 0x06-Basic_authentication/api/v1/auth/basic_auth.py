f5bf664e8d2fed2521106945a9e25bb6f864df1d
Alejandro Pineda Sanchez <apsanchez2549@gmail.com>
2023-05-31 14:04:13 -0500

    feat: Added base64 extract

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,4 +6,10 @@ from .auth import Auth
 
 class BasicAuth(Auth):
     """Class Basic Auth"""
-    pass
+    def extract_base64_authorization_header(self, authorization_header: str):
+        '''Extracts base 64 from auth_header'''
+        if authorization_header is None\
+           or type(authorization_header) is not str\
+           or not authorization_header.startswith("Basic "):
+            return None
+        return authorization_header[6:]
