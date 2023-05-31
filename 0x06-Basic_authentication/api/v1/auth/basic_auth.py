a38049bff9e6adb2218a4987d2a048eb51887783
Alejandro Pineda Sanchez <apsanchez2549@gmail.com>
2023-05-31 14:32:14 -0500

    feat: Added 8

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """Basic auth module"""
 
 from .auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -9,7 +10,19 @@ class BasicAuth(Auth):
     def extract_base64_authorization_header(self, authorization_header: str):
         '''Extracts base 64 from auth_header'''
         if authorization_header is None\
-           or type(authorization_header) is not str\
-           or not authorization_header.startswith("Basic "):
+                or type(authorization_header) is not str\
+                or not authorization_header.startswith("Basic "):
             return None
         return authorization_header[6:]
+    
+
+    def decode_base64_authorization_header(self, base64_authorization_header: str):
+        '''Decodes base64'''
+        if not base64_authorization_header\
+                or type(base64_authorization_header) is not str:
+            return None
+        to_decode = base64_authorization_header.encode('utf-8')
+        try:
+            return base64.b64decode(to_decode).decode('utf-8')
+        except Exception:
+            return None
