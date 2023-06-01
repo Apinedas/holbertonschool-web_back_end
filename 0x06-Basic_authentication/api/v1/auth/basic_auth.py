8396c7bd5e8aa8d1ff47f5e88d21976f7ec1e896
Alejandro Pineda Sanchez <apsanchez2549@gmail.com>
2023-05-31 22:07:34 -0500

    feat: Added User manipulation

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,8 @@
 
 from .auth import Auth
 import base64
+from typing import TypeVar
+from ..views.users import User
 
 
 class BasicAuth(Auth):
@@ -33,3 +35,13 @@ class BasicAuth(Auth):
                 ":" not in decoded_base64_authorization_header:
             return (None, None)
         return tuple(decoded_base64_authorization_header.split(":"))
+
+    def user_object_from_credentials(self, user_email: str, user_pwd: str) ->\
+    TypeVar('User'):
+        '''Get user object from credentials'''
+        list_user_from_db = User.search({'email': user_email})
+        if type(user_email) is not str or type(user_pwd) is not str or\
+                len(list_user_from_db) == 0 or not\
+                list_user_from_db[0].is_valid_password(user_pwd):
+            return None
+        return list_user_from_db[0]
